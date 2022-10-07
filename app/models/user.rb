f14a8d4f3ff7724d2be31a2b696b5c8b3bf80d87
kitano <onatik0198@gmail.com>
2022-10-07 12:10:09 +0900

    ノーパソ版carriawave導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }, on: :create, on: :create
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
   has_secure_password
 end
