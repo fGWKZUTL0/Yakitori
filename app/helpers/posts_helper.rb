019f70da6205d56fc02d2a361523c25ef6ea6448
fGWKZUTL0 <onatik0198@gmail.com>
2022-10-26 01:14:49 +0900

    リプライ機能実装（非Ajax）

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def reply_count(post)
+    Post.all.where(parent_id: post.id).count
+  end
+
+  def get_childs(post)
+    Post.all.where(parent_id: post.id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true #関連名
 
   has_many :likes, dependent: :destroy
+  has_many :posts
+  
   def liked_by?(user)
     likes.exists?(user_id: user.id)
   end
+
 end
