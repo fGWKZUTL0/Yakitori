544a3ec403a4350552c08319c7f0f38e60a8fedd
fGWKZUTL0 <onatik0198@gmail.com>
2022-11-05 13:00:06 +0900

    replyのturbo化-ついに完成（デザインはゴミだけど）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,29 +23,31 @@ class PostsController < ApplicationController
 
   def create
     @this_user = User.find_by(username: params[:username])
-
-    if params[:parent_id] == nil || params[:parent_id] == "-1"
+    users = User.all
+    if params[:parent_id] == nil || params[:parent_id].to_i == -1
       #リプライではない普通のツイート
       @post = Post.new(username: params[:username], content: params[:content], parent_id: -1)
       @post.save
 
-      render turbo_stream: turbo_stream.replace(
-        "new_post",
-        partial: 'shared/new_post',
+      render turbo_stream: turbo_stream.prepend(
+        "timeline",
+        partial: 'shared/post',
         locals: { post: @post, this_user: @this_user},
       )
     else
       #リプライ
       @post = Post.new(username: params[:username], content: params[:content], parent_id: params[:parent_id])
       @post.save
-      parent_post = Post.find_by(id: params[:parent_id])
+      parent_post = Post.find_by(id: getEndOfParentId(params[:parent_id]))
       parent_post_user = User.find_by(username: parent_post.username)
 
-      render turbo_stream: turbo_stream.replace(
-        "turbo-frame-post-#{params[:parent_id]}",
-        partial: 'shared/post',
-        locals: { post: parent_post, this_user: parent_post_user},
-      )
+      render turbo_stream: [
+        turbo_stream.replace(
+          "turbo-frame-reply-posts",
+          partial: 'shared/reply_posts',
+          locals: { post: parent_post, this_user: parent_post_user, users: users},
+        )
+      ]
     end
     #redirect_to("/posts/index")
   end
@@ -80,9 +82,10 @@ class PostsController < ApplicationController
       @users = User.where(id: followed_id_lists).or(User.where(id: user_id))
     end
 
-    def getEndOfParentId(post)
-      while post.parent_id != -1
-        post = Post.all.find_by(id: post.parent_id)
+    def getEndOfParentId(id)
+      post = Post.find_by(id: id)
+      while post.parent_id != -1 do
+        post = Post.find_by(id: post.parent_id)
       end
       post.id
     end
